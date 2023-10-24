bada148a22c6cc140e1e7c5e46a44986dd1f7fb7
Ichiky_Otsu <66952647+IchikyOtsu@users.noreply.github.com>
2023-10-24 13:54:25 +0200

    Update prog.c
    changement adresse relative

--- UseCSV/FichierH/prog.c
+++ UseCSV/FichierH/prog.c
@@ -1,25 +1,25 @@
-#include "pilotes.h"
-
+//#include "/home/ichiky/Documents/Merced'OS/UseCSV/FichierH/utils/pilotes.h"
+#include "utils/pilotes.h"
 int main() {
     struct Joueur joueurs[MAX_LINES];
     int ligneIndex = 0;
 
     // Lire les données à partir du fichier CSV
-    if (lireFichierCSV("pilotes.csv", joueurs, &ligneIndex) != 0) {
+    if (lireFichierCSV("data/pilotes.csv", joueurs, &ligneIndex) != 0) {
         return 1;
     }
-
+    int temps1 = 500;
+	joueurs[6].Q1 = temps1;
     // Ajouter de nouvelles données au tableau (par exemple, ajouter un nouveau joueur)
-    strcpy(joueurs[ligneIndex].Prenom, "Nouveau");
+    /*strcpy(joueurs[ligneIndex].Prenom, "Nouveau");
     strcpy(joueurs[ligneIndex].Nom, "Joueur");
     joueurs[ligneIndex].nb = 23;
-    ligneIndex++;
+    ligneIndex++;*/
 
     // Sauvegarder les données dans le fichier CSV
-    if (sauvegarderFichierCSV("pilotes.csv", joueurs, ligneIndex) != 0) {
+    if (sauvegarderFichierCSV("data/pilotes.csv", joueurs, ligneIndex) != 0) {
         return 1;
     }
 
     return 0;
 }
-
