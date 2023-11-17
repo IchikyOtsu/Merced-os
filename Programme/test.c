3f09da1f7262dc16c2c84d80242fa36f11953aa4
IchikyOtsu <bricedelcroix20@gmail.com>
2023-11-17 21:56:26 +0100

    genererTempsAleatoires() prend un min et max et renvoie pour le joueur un temps S1 /S2 /S3

--- Programme/test.c
+++ Programme/test.c
@@ -1,5 +1,6 @@
 //#include "/home/ichiky/Documents/Merced'OS/UseCSV/FichierH/utils/pilotes.h"
 #include "utilities/pilote.h"
+#include "utilities/random.h"
 int main() {
     struct Joueur joueurs[MAX_LINES];
     int ligneIndex = 0;
@@ -7,6 +8,17 @@ int main() {
         fprintf(stderr, "Erreur de lecture du fichier CSV.\n");
         return 1 ;  // Sortir de la fonction en cas d'erreur
     }
+    float tempsSecteurs[4];
+    genererTempsAleatoires(20001, 25000, tempsSecteurs); // Utilisez min = 20001 ms et max = 25000 ms
+
+    // Afficher les temps
+    printf("S1: %.3f, S2: %.3f, S3: %.3f, Tour: %.3f\n", 
+           tempsSecteurs[0], tempsSecteurs[1], tempsSecteurs[2], tempsSecteurs[3]);
+
+
+
+
+
     if (sauvegarderFichierCSV("data/pilotes.csv", joueurs, ligneIndex) != 0) {
         return 1;
     }
--- Programme/utilities/random.h
+++ Programme/utilities/random.h
@@ -1,5 +1,5 @@
-#ifndef RANDOMMILI_H
-#define RANDOMMILI_H
+#ifndef RANDOM_H
+#define RANDOM_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,7 +11,7 @@
  * @param nbrTours Le nombre de tours pour lesquels générer des temps aléatoires.
  * @return Le temps minimum trouvé parmi tous les tours, en secondes.
  */
-float trouver_temps_minimum(int nbrTours) {
+float zdds(int nbrTours) {
     //srand(time(NULL));  // Initialisation du générateur de nombres aléatoires
     /*L'appel à srand(time(NULL)) est effectué à chaque fois que la fonction trouver_temps_minimum est appelée. Cela réinitialise le générateur de nombres aléatoires avec une nouvelle graine à chaque appel. Si ces appels sont rapprochés dans le temps (par exemple, lors de la simulation rapide de plusieurs pilotes), time(NULL) pourrait retourner la même valeur, ce qui entraînerait des séquences de nombres aléatoires identiques et donc des temps très similaires pour différents pilotes.*/
     float min_temps = 45.0;  // Initialisation du temps minimum à 45 secondes
@@ -28,4 +28,21 @@ float trouver_temps_minimum(int nbrTours) {
     return min_temps;
 }
 
-#endif  // RANDOMMILI_H
+
+void genererTempsAleatoires(int min, int max, float temps[4]) {
+    // Initialiser le générateur de nombres aléatoires
+    srand(time(NULL));
+
+    // Générer des temps aléatoires pour chaque secteur
+    temps[0] = (rand() % (max - min + 1) + min) / 1000.0f; // S1
+    temps[1] = (rand() % (max - min + 1) + min) / 1000.0f; // S2
+    temps[2] = (rand() % (max - min + 1) + min) / 1000.0f; // S3
+
+    // Calculer le temps total du tour
+    temps[3] = temps[0] + temps[1] + temps[2]; // Tour
+}
+
+
+
+
+#endif  // RANDOM_H
