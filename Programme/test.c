be146077520c95a25cf53945b75ff58d57e78206
IchikyOtsu <bricedelcroix20@gmail.com>
2023-11-17 21:38:40 +0100

    modification de Pilote.h pour les nouvelles collonnes et passage de .2f à .3f

--- Programme/test.c
+++ Programme/test.c
@@ -1,9 +1,13 @@
 //#include "/home/ichiky/Documents/Merced'OS/UseCSV/FichierH/utils/pilotes.h"
-#include "utilities/randommili.h"
+#include "utilities/pilote.h"
 int main() {
-    int nombreDeTours = 60;
-    float temps_min = trouver_temps_minimum(nombreDeTours);
-    printf("Le temps minimum sur %d tours est : %.3f secondes\n", nombreDeTours, temps_min);
-
-    return 0;
+    struct Joueur joueurs[MAX_LINES];
+    int ligneIndex = 0;
+    if (lireFichierCSV("./data/pilotes.csv", joueurs, &ligneIndex) != 0) {
+        fprintf(stderr, "Erreur de lecture du fichier CSV.\n");
+        return 1 ;  // Sortir de la fonction en cas d'erreur
+    }
+    if (sauvegarderFichierCSV("data/pilotes.csv", joueurs, ligneIndex) != 0) {
+        return 1;
+    }
 }
--- Programme/utilities/pilote.h
+++ Programme/utilities/pilote.h
@@ -6,7 +6,7 @@
 #include <string.h>
 
 #define MAX_LINES 100  
-#define MAX_FIELDS 10 
+#define MAX_FIELDS 38 
 
 struct Joueur {
     int nb;
@@ -16,12 +16,39 @@ struct Joueur {
     char Team[50];
     char Naissance[15];
     char Nationalite[30];
+    float S1P1;
+    float S2P1;
+    float S3P1;
     float P1;
+
+    float S1P2;
+    float S2P2;
+    float S3P2;
     float P2;
+
+    float S1P3;
+    float S2P3;
+    float S3P3;
     float P3;
+
+    float S1Q1;
+    float S2Q1;
+    float S3Q1;
     float Q1;
+
+    float S1Q2;
+    float S2Q2;
+    float S3Q2;
     float Q2;
+
+    float S1Q3;
+    float S2Q3;
+    float S3Q3;
     float Q3;
+
+    float S1R;
+    float S2R;
+    float S3R;
     float RACE;
     
 };
@@ -88,24 +115,101 @@ int lireFichierCSV(const char *nomFichier, struct Joueur joueurs[], int *ligneIn
                     strcpy(joueurs[*ligneIndex].Nationalite, token);
                     break;
                 case 7:
-                    joueurs[*ligneIndex].P1= atoi(token);
+                    joueurs[*ligneIndex].S1P1= atoi(token);
                     break;
                 case 8:
-                    joueurs[*ligneIndex].P2= atoi(token);
+                    joueurs[*ligneIndex].S2P1= atoi(token);
                     break;
                 case 9:
-                    joueurs[*ligneIndex].P3= atoi(token);
+                    joueurs[*ligneIndex].S3P1= atoi(token);
                     break;
                 case 10:
-                    joueurs[*ligneIndex].Q1= atoi(token);
+                    joueurs[*ligneIndex].P1= atoi(token);
                     break;
                 case 11:
-                    joueurs[*ligneIndex].Q2= atoi(token);
+                    joueurs[*ligneIndex].S1P2= atoi(token);
                     break;
                 case 12:
-                    joueurs[*ligneIndex].Q3= atoi(token);
+                    joueurs[*ligneIndex].S2P2= atoi(token);
                     break;
                 case 13:
+                    joueurs[*ligneIndex].S3P2= atoi(token);
+                    break;
+                case 14:
+                    joueurs[*ligneIndex].P2= atoi(token);
+                    break;
+                case 15:
+                    joueurs[*ligneIndex].S1P3= atoi(token);
+                    break;
+                case 16:
+                    joueurs[*ligneIndex].S2P3= atoi(token);
+                    break;
+                case 17:
+                    joueurs[*ligneIndex].S3P3= atoi(token);
+                    break;
+                case 18:
+                    joueurs[*ligneIndex].P3= atoi(token);
+                    break;
+                
+                case 19:
+                    joueurs[*ligneIndex].S1Q1= atoi(token);
+                    break;
+                case 20:
+                    joueurs[*ligneIndex].S2Q1= atoi(token);
+                    break;
+                case 21:
+                    joueurs[*ligneIndex].S3Q1= atoi(token);
+                    break;
+                case 22:
+                    joueurs[*ligneIndex].Q1= atoi(token);
+                    break;
+
+                case 23:
+                    joueurs[*ligneIndex].S1Q2= atoi(token);
+                    break;
+                case 24:
+                    joueurs[*ligneIndex].S2Q2= atoi(token);
+                    break;
+                case 25:
+                    joueurs[*ligneIndex].S3Q2= atoi(token);
+                    break;
+
+                case 26:
+                    joueurs[*ligneIndex].S1Q2= atoi(token);
+                    break;
+                case 27:
+                    joueurs[*ligneIndex].S2Q2= atoi(token);
+                    break;
+                case 28:
+                    joueurs[*ligneIndex].S3Q2= atoi(token);
+                    break;
+                case 29:
+                    joueurs[*ligneIndex].Q2= atoi(token);
+                    break;
+
+                case 30:
+                    joueurs[*ligneIndex].S1Q3= atoi(token);
+                    break;
+                case 31:
+                    joueurs[*ligneIndex].S2Q3= atoi(token);
+                    break;
+                case 32:
+                    joueurs[*ligneIndex].S3Q3= atoi(token);
+                    break;
+                case 33:
+                    joueurs[*ligneIndex].Q3= atoi(token);
+                    break;
+                
+                case 34:
+                    joueurs[*ligneIndex].S1R= atoi(token);
+                    break;
+                case 35:
+                    joueurs[*ligneIndex].S2R= atoi(token);
+                    break;
+                case 36:
+                    joueurs[*ligneIndex].S3R= atoi(token);
+                    break;
+                case 37:
                     joueurs[*ligneIndex].RACE= atoi(token);
                     break;
                 default:
@@ -124,27 +228,56 @@ int lireFichierCSV(const char *nomFichier, struct Joueur joueurs[], int *ligneIn
 // Fonction pour sauvegarder un tableau de structures dans un fichier CSV
 int sauvegarderFichierCSV(const char *nomFichier, struct Joueur joueurs[], int ligneIndex) {
     FILE *fichier = fopen(nomFichier, "w");
-
     if (fichier == NULL) {
         perror("Erreur lors de l'ouverture du fichier");
         return 1;
     }
 
-    fprintf(fichier, "nb,Num,Prénom,Nom,Team,Naissance,Nationalité\n");
+    // Mise à jour de l'en-tête pour inclure tous les champs
+    fprintf(fichier, "nb,Num,Prenom,Nom,Team,Naissance,Nationalite,S1P1,S2P1,S3P1,P1,S1P2,S2P2,S3P2,P2,S1P3,S2P3,S3P3,P3,S1Q1,S2Q1,S3Q1,Q1,S1Q2,S2Q2,S3Q2,Q2,S1Q3,S2Q3,S3Q3,Q3,S1R,S2R,S3R,RACE\n");
 
     for (int i = 0; i < ligneIndex; i++) {
-        if (joueurs[i].Prenom[0] != '\0' && joueurs[i].Nom[0] != '\0') {
-            fprintf(fichier, "%d,%d,%s,%s,%s,%s,%s,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f\n", 
-        joueurs[i].nb, joueurs[i].Num, joueurs[i].Prenom, joueurs[i].Nom, 
-        joueurs[i].Team, joueurs[i].Naissance, joueurs[i].Nationalite,
-        joueurs[i].P1, joueurs[i].P2, joueurs[i].P3, 
-        joueurs[i].Q1, joueurs[i].Q2, joueurs[i].Q3, 
-        joueurs[i].RACE);
-}
+        fprintf(fichier, "%d,%d,%s,%s,%s,%s,%s,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f\n", 
+            joueurs[i].nb, 
+            joueurs[i].Num, 
+            joueurs[i].Prenom, 
+            joueurs[i].Nom, 
+            joueurs[i].Team, 
+            joueurs[i].Naissance, 
+            joueurs[i].Nationalite,
+            joueurs[i].S1P1, 
+            joueurs[i].S2P1, 
+            joueurs[i].S3P1, 
+            joueurs[i].P1,
+            joueurs[i].S1P2, 
+            joueurs[i].S2P2, 
+            joueurs[i].S3P2, 
+            joueurs[i].P2,
+            joueurs[i].S1P3, 
+            joueurs[i].S2P3, 
+            joueurs[i].S3P3, 
+            joueurs[i].P3,
+            joueurs[i].S1Q1, 
+            joueurs[i].S2Q1, 
+            joueurs[i].S3Q1, 
+            joueurs[i].Q1,
+            joueurs[i].S1Q2, 
+            joueurs[i].S2Q2, 
+            joueurs[i].S3Q2, 
+            joueurs[i].Q2,
+            joueurs[i].S1Q3, 
+            joueurs[i].S2Q3, 
+            joueurs[i].S3Q3, 
+            joueurs[i].Q3,
+            joueurs[i].S1R, 
+            joueurs[i].S2R, 
+            joueurs[i].S3R, 
+            joueurs[i].RACE);
     }
 
     fclose(fichier);
     return 0;
 }
+
 
 #endif
