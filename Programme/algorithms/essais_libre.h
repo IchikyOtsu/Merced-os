f65e769670bfbebb23bb4ab310f86fbc57e2bb9d
IchikyOtsu <bricedelcroix20@gmail.com>
2023-11-24 19:55:38 +0100

    Fork /semaphore / meilleur temps sauvegarder

--- Programme/algorithms/essais_libre.h
+++ Programme/algorithms/essais_libre.h
@@ -1,22 +1,23 @@
 #ifndef ESSAIS_LIBRE_H
 #define ESSAIS_LIBRE_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <semaphore.h>
 #include "../utilities/pilote.h"
 #include "../utilities/random.h"
 #include "../utilities/drive.h"
 
-
 // Définir NOMBRE_DE_TOURS quelque part dans votre code
-
-
-
-
 #define NOMBRE_DE_TOURS 60
 
-
+// Déclaration d'un sémaphore
+sem_t sharedMemorySemaphore;
 
 int sessionEssaisLibres(float nbrTours) {
-
-        // Utilisez la même clé que dans le programme principal
+    // Utilisez la même clé que dans le programme principal
     key_t key = ftok("data/shmkeyfile", 65);
     if (key == -1) {
         perror("ftok main");
@@ -31,58 +32,94 @@ int sessionEssaisLibres(float nbrTours) {
     }
 
     // Attachement de la mémoire partagée
-    
     struct Joueur *resultats = (struct Joueur *)shmat(shmid, NULL, 0);
     if ((intptr_t)resultats == -1) {
         perror("shmat");
         exit(1);
     }
-    //printf("test mémoire partager%s\n", resultats[2].Nom);
-    
-    
-    
-    for(int tour = 0; tour < nbrTours; tour++){
 
-        // faire rouller chaque joueur ses 60 tours et récuperer ses meilleurs temps dans un tableau (S1, S2, S3 et le meilleur tour)
+    // Initialisation du sémaphore pour la mémoire partagée
+    if (sem_init(&sharedMemorySemaphore, 0, 1) != 0) {
+        perror("sem_init");
+        exit(1);
+    }
+
+    for (int tour = 0; tour < nbrTours; tour++) {
+        // faire rouler chaque joueur ses 60 tours et récupérer ses meilleurs temps
         for (int i = 0; i < MAX_LINES; i++) {
-        
-            
-            float *meilleursTemps = drive(60);
-            
-            printf("\n------joueur %d -------\n",i);
-            printf("ID du joueur : %d\n", resultats[i].nb);
-            printf("Nom du joueur : %s\n", resultats[i].Nom);
-            printf("temps S1 : %f\n", meilleursTemps[0]);  
-            printf("temps S2 : %f\n", meilleursTemps[1]);
-            printf("temps S3 : %f\n", meilleursTemps[2]);
-            printf("temps T1 : %f\n", meilleursTemps[3]);
-            
-            
-            
-            //sauve ces inforamtion dans la mémoire partagée
-        resultats[i].S1P1 = meilleursTemps[0];
-        resultats[i].S2P1 = meilleursTemps[1];
-        resultats[i].S3P1 = meilleursTemps[2];
-        resultats[i].P1 = meilleursTemps[3];
-        
+            pid_t pid = fork();
+            if (pid == -1) {
+                // Gestion d'erreur
+                perror("fork failed");
+                exit(1);
+            } else if (pid == 0) {
+                float *meilleursTemps = drive(60);
+
+                
+                
+
+                printf("\n------joueur %d -------\n", i + 1);
+                printf("ID du joueur : %d\n", resultats[i].nb);
+                printf("Nom du joueur : %s\n", resultats[i].Nom);
+                printf("temps S1 : %f\n", meilleursTemps[0]);
+                printf("temps S2 : %f\n", meilleursTemps[1]);
+                printf("temps S3 : %f\n", meilleursTemps[2]);
+                printf("temps T1 : %f\n", meilleursTemps[3]);
+
+                
+                sem_wait(&sharedMemorySemaphore);
+                if (meilleursTemps[0] < resultats[i].S1P1) {
+                    // Mettez à jour le temps uniquement s'il est plus petit
+                    resultats[i].S1P1 = meilleursTemps[0];
+                }
+                if (meilleursTemps[1] < resultats[i].S2P1) {
+                    // Mettez à jour le temps uniquement s'il est plus petit
+                    resultats[i].S2P1 = meilleursTemps[1];
+                }
+                if (meilleursTemps[2] < resultats[i].S3P1) {
+                    // Mettez à jour le temps uniquement s'il est plus petit
+                    resultats[i].S3P1 = meilleursTemps[2];
+                }
+                if (meilleursTemps[3] < resultats[i].P1) {
+                    // Mettez à jour le temps uniquement s'il est plus petit
+                    resultats[i].P1 = meilleursTemps[3];
+                }
+
+                /*// Sauvegarde de ces informations dans la mémoire partagée
+                resultats[i].S1P1 = meilleursTemps[0];
+                resultats[i].S2P1 = meilleursTemps[1];
+                resultats[i].S3P1 = meilleursTemps[2];
+                resultats[i].P1 = meilleursTemps[3];
+                */
+
+                // Libérez le sémaphore après avoir effectué les opérations sur la mémoire partagée
+                sem_post(&sharedMemorySemaphore);
+
+                _exit(0);
+            }
+            srand(time(NULL));
+            usleep(650*1000);
         }
 
-	// Sauvegarde dans le fichier CSV
-	if (sauvegarderFichierCSV("data/pilotes.csv", resultats, MAX_LINES) != 0) {
+        for (int i = 0; i < MAX_LINES; i++) {
+            wait(NULL);
+        }
+    }
+
+    // Sauvegarde dans le fichier CSV
+    if (sauvegarderFichierCSV("data/pilotes.csv", resultats, MAX_LINES) != 0) {
         fprintf(stderr, "Erreur de sauvegarde du fichier CSV.\n");
         return 1;
     }
-    }
-    
-    
-    
-        // Détacher le segment \\est ce qu'on doit le faire à chaque fois que l'on lance la mémoire partager car j'ai lu que cela décrémente le liens ou bien en le fesant une seule fois cela suffit pour la suprimer ?
+
+    // Détacher le segment de mémoire partagée
     if (shmdt(resultats) == -1) {
         perror("shmdt");
         exit(1);
     }
-    
-}
 
+    // Détruire le sémaphore
+    sem_destroy(&sharedMemorySemaphore);
+}
 
 #endif
